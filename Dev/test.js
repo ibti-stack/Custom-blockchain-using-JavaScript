adcf06813ebff143c992ea50a192fafdcca2a045
ibti-stack <[ahmed.ibtisam@yahoo.com]>
2021-04-15 21:20:17 +0200

    Change the cons. func. from test file to blockchain.js file

--- Dev/test.js
+++
@@ -1,4 +0,0 @@
-function Blockchain() {
-    this.chain = [];              // All blocks will be stored in this array.
-    this.newTransactions = [];    // Placed all the transactions into this array before they are mine.
-}
