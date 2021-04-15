67b37d7722809d2618ae21e90b71d5aa4999be06
ibti-stack <[ahmed.ibtisam@yahoo.com]>
2021-04-15 22:06:45 +0200

    Created a create new transaction method

--- Dev/blockchain.js
+++ Dev/blockchain.js
@@ -1,19 +1,19 @@
 function Blockchain() {
     this.chain = [];              // All blocks will be stored in this array.
-    this.newTransactions = [];    // Placed all the transactions into this array before they are mine.
+    this.pendingTransactions = [];    // Placed all the transactions into this array before they are mine.
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transaction: this.newTransactions,
+        transaction: this.pendingTransactions,
         nonce: nonce,              // Nonce comes from POW and it could be any number.
         hash: hash,                //Hash of the new block. We will pass the new transactions into a hashing function.
         previousBlockHash: previousBlockHash
     }
 
-    this.newTransactions = [];            // Clear the array so that we can do the process again.
+    this.pendingTransactions = [];            // Clear the array so that we can do the process again.
     this.chain.push(newBlock);
 
     return newBlock;
@@ -21,6 +21,16 @@ Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) 
 
 Blockchain.prototype.getLastBlock = function() {       // Why this method? Ans: We need it for previous block hash.
     return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipent) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipent: recipent
+    }
+
+    this.pendingTransactions.push(newTransaction);
 }
 
 module.exports = Blockchain;
