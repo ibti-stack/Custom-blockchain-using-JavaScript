37c4944b5261f84b3bef7725618bc8b9b5370dec
ibti-stack <[ahmed.ibtisam@yahoo.com]>
2021-04-17 13:36:47 +0200

    Building the API foundation

---
+++ Dev/api.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+ 
+app.get('/blockchain', function (req, res) {
+  
+});
+
+app.post('/transaction', function(req, res){
+
+});
+
+app.get('/mine', function (req, res) {
+  
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log('LIstening on port 3000....');
+});
