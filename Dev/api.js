5fad39f76b5f605a30fdfd12cd33d939fb36e781
ibti-stack <[ahmed.ibtisam@yahoo.com]>
2021-04-17 14:01:28 +0200

    Created diff. API endpoints and tested on postman

--- Dev/api.js
+++ Dev/api.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const app = express()
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
  
 app.get('/blockchain', function (req, res) {
   
 });
 
 app.post('/transaction', function(req, res){
-
+    console.log(req.body);
+    res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
 });
 
 app.get('/mine', function (req, res) {
@@ -18,4 +23,4 @@ app.get('/mine', function (req, res) {
 
 app.listen(3000, function(){
     console.log('LIstening on port 3000....');
-});
+}); 
