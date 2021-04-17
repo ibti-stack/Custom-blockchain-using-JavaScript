856c89af774ff7728d0a1a75e1d7ee9d71c7da67
ibti-stack <[ahmed.ibtisam@yahoo.com]>
2021-04-17 14:50:43 +0200

    Made a Get/blockchain endpoint

--- Dev/api.js
+++ Dev/api.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain.js');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
  
 app.get('/blockchain', function (req, res) {
-  
+    res.send(bitcoin);
 });
 
 app.post('/transaction', function(req, res){
@@ -17,8 +20,6 @@ app.post('/transaction', function(req, res){
 app.get('/mine', function (req, res) {
   
 });
-
-
 
 
 app.listen(3000, function(){
