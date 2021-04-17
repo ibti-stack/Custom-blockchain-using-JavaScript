c8b4adf7ec7809b532cabe65d93d9c1698074154
ibti-stack <[ahmed.ibtisam@yahoo.com]>
2021-04-17 11:46:03 +0200

    Testes a hash block method

--- Dev/test.js
+++ Dev/test.js
@@ -1,12 +1,20 @@
 const Blockchain = require('./blockchain');
 
 
-const bitcoin = new Blockchain();
+const bitcoin = new Blockchain();            //Instance of our Blockchain
 
-bitcoin.createNewBlock("123456", "shfvysyyfsfsffsfs", "dhsssssvgsgvsgvsg");
-
-bitcoin.createNewTransaction("10$","ALEXghghghghg","JENNgsggssgsg");
-
-bitcoin.createNewBlock("654321", "ghghghghghghghghg", "kjkjkjkjkjkjkj");
-
-console.log(bitcoin.chain[1]);
+const previousBlockHash = "OINAISDGHGHGHGHHGHGHGH";
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: "SENDERRRRRRR",
+        recipent: "RECIPENTTTTTTTT"
+    },
+    {
+        amount: 30,
+        sender: "SENDERRRRRRRADDRESS",
+        recipent: "RECIPENTTTTTTTTADDRESD"
+    }
+];
+const nonce = 100;
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
