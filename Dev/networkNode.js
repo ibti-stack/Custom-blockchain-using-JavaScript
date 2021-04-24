a82912a0053719f4eb5b831941f85148efd3f774
ibti-stack <[ahmed.ibtisam@yahoo.com]>
2021-04-24 15:59:06 +0200

    Updated the mining endpoint so that it broadcast the mine block to all other nodes

--- Dev/networkNode.js
+++ Dev/networkNode.js
@@ -55,14 +55,41 @@ app.get('/mine', function (req, res) {
 
     const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
-    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+    
 
     const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
 
-    
-    res.json({
-        note: "New block mined successfully",
-        block: newBlock
+    const requestPromises = [];
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: networkNodeUrl + '/recieve-new-block',
+            method: 'POST',
+            body: { newBlock: newBlock },
+            json: true
+        };
+        requestPromises.push(rp(requestOptions));
+    });
+
+    Promise.all(requestPromises)
+    .then(data => {
+        const requestOptions = {
+            uri: bitcoin.currentNodeUrl + '/transaction/broadcast',
+            method: 'POST',
+            body: { 
+                amount: 12.5,
+                sender: "00",
+                recipent: nodeAddress
+            },
+            json: true
+        };
+
+       return rp(requestOptions);
+    })
+    .then(data => {
+        res.json({
+            note: "New block mined successfully",
+            block: newBlock
+        });
     });
 });
 
